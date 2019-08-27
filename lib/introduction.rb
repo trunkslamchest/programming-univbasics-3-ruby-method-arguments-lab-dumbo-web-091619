6cc5171ab074c4fac3936e35b36d8cfdcc44655f
Austin Smith <trunk9slamchest@gmail.com>
2019-08-27 14:33:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,7 @@ introduction("Dan")
 
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in Ember.js."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language("Dan", "Ember.js")
